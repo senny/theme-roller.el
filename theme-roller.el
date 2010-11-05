a174c19ff9ca08b7326738adb40b3e67125aa450
Yves Senn <yves.senn@gmail.com>
2010-11-05 14:45:48 +0100

    + credits

--- theme-roller.el
+++ theme-roller.el
@@ -28,6 +28,10 @@
 ;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 ;; Boston, MA 02110-1301, USA.
 
+;;; Contributors
+;;
+;;  - Christoph Ritler <christoph ritler * garaio com>
+
 ;;; Commentary:
 
 ;; (color-theme-lazy)
