e7cc939399ae6ad59f096870e4e19d82ade1c8d3
Yves Senn <yves.senn@gmail.com>
2010-11-13 18:14:11 +0100

    . more tweaks for monokai

--- themes/monokai.el
+++ themes/monokai.el
@@ -4,7 +4,7 @@
 ;;
 (theme-roller-make-theme
  "Monokai"
- ((background-color . "#272822")
+ ((background-color . "#161711")
   (background-mode . dark)
   (border-color . "black")
   (cursor-color . "#F8F8F0")
@@ -17,14 +17,14 @@
  :link               (:foreground "blue1" :underline t)
  :mode-line          (:background "#bcd5fa" :foreground "black")
  :minibuffer-prompt  (:inherit theme-roller--code-keyword)
- :code-variable      (:foreground "#66D9EF")
+ :code-variable      (:foreground "#FD971F")
  :code-reference     (nil)
  :code-constant      (:foreground "#AE81FF")
  :code-type          (:foreground "#A6E22E")
- :code-keyword       (:weight bold :foreground "#F92672")
+ :code-keyword       (:foreground "#F92672")
  :code-string        (:foreground "#A6E22E")
  :code-comment       (:foreground "#75715E")
- :code-documentation (:foreground "#F67B37")
+ :code-documentation (:foreground "#66D9EF")
  :code-function      (:foreground "#E6DB74")
  :code-meta          (:foreground "#8996A8")
  :code-error         (:background "#F92672" :foreground "#F8F8F0")
@@ -32,7 +32,7 @@
  ;; TODO: style diff faces
  :diff-removed       (:background "#420E09" :foreground "#F8F8F8")
  :diff-changed       (:background "#4A410D" :foreground "#F8F8F8")
- :diff-info          (:background "#0E2231" :italic t :foreground "#F8F8F8")
+ :diff-info          (:background "#0E2231" :foreground "#F8F8F8" :italic t)
  :diff-added         (:background "#253B22" :foreground "#F8F8F8")
  :diff-context       (:inherit theme-roller--base-font)
  :code-escape-char   (:foreground "#FF8000"))
